4d0616b859c202fabd300b63556b9ba5dcca8130
Krunal Patel <krunal@silverwebbuzz.com>
2024-02-04 16:24:51 +0530

    GST Report Export

--- app/Http/Controllers/API/ExcelController.php
+++ app/Http/Controllers/API/ExcelController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\ExcalExportClass;
 use App\Exports\GstDetaliExport;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
 
 class ExcelController extends Controller
 {
@@ -23,16 +25,30 @@ class ExcelController extends Controller
         $report = 'gstr2';
         return Excel::download(new ExcalExportClass($year, $month, $last_six_month, $report), $file_name);
     }
-    public function download_excel_gstr1_detail($start_date, $end_date)
+    public function download_excel_gstr1_detail()
     {
-        $file_name = 'GSTR1_detail' . $start_date  . '_to_' . $end_date . '.xlsx';
+        $start_date =  !empty($_GET['start_date']) ? $_GET['start_date'] : Carbon::now()->startOfMonth()->format('Y-m-d');;
+        $end_date = !empty($_GET['end_date'])? $_GET['end_date'] :  Carbon::now()->format('Y-m-d');
+        $time = Carbon::now()->format('H_i_s');
+        $file_name = 'GSTR1_detail_' . str_replace("-","_",$start_date)  . '_to_' . str_replace("-","_",$end_date) . '_' . $time . '.xlsx';
         $report = 'gstr1_detail';
-        return Excel::download(new GstDetaliExport($start_date, $end_date, $report), $file_name);
-    }
-    public function download_excel_gstr2_detail($start_date, $end_date)
+
+        $diskPath = 'gstr1_detail';
+        
+        Storage::disk('public')->put($diskPath . '/' .$file_name, Excel::raw(new GstDetaliExport($start_date, $end_date, $report), \Maatwebsite\Excel\Excel::XLSX));
+        return response()->json(["statusCode" => 0, "message" => $file_name . " file imported sucessfully,"], 200);
+    }    
+    public function download_excel_gstr2_detail()
     {
-        $file_name = 'GSTR2_detail' . $start_date  . '_to_' . $end_date . '.xlsx';
+        $start_date =  !empty($_GET['start_date']) ? $_GET['start_date'] : Carbon::now()->startOfMonth()->format('Y-m-d');;
+        $end_date = !empty($_GET['end_date'])? $_GET['end_date'] :  Carbon::now()->format('Y-m-d');
+        $time = Carbon::now()->format('H_i_s');
+        $file_name = 'GSTR2_detail_' . str_replace("-","_",$start_date)  . '_to_' . str_replace("-","_",$end_date) . '_' . $time . '.xlsx';
         $report = 'gstr2_detail';
-        return Excel::download(new GstDetaliExport($start_date, $end_date, $report), $file_name);
+
+        $diskPath = 'gstr2_detail';
+        
+        Storage::disk('public')->put($diskPath . '/' .$file_name, Excel::raw(new GstDetaliExport($start_date, $end_date, $report), \Maatwebsite\Excel\Excel::XLSX));
+        return response()->json(["statusCode" => 0, "message" => $file_name . " file imported sucessfully,"], 200);
     }
 }
